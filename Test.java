6601dbef05d9652e3d55ed743ce5656c6d83146d
praveen-kumar-maurya <88583491+praveen-kumar-maurya@users.noreply.github.com>
2021-08-10 23:09:17 +0530

    updated

--- Test.java
+++ Test.java
@@ -1,9 +1,7 @@
 public class Test{
-<<<<<<< HEAD:Test.java
 public static void main(String args[]){
-=======
   // printing hello world
-public statis void main(String args[]){
+public static void main(String args[]){
 >>>>>>> 41906a76c3a351719f6834a75c00ad749e8c3599:hellow_world.java
 System.out.println("hello world");
 //   added one more commment
