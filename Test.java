a300a77a2bca9380a207372d9ad9f807973b1705
praveen-kumar-maurya <88583491+praveen-kumar-maurya@users.noreply.github.com>
2021-08-10 23:09:53 +0530

    updated

--- Test.java
+++ Test.java
@@ -1,8 +1,6 @@
 public class Test{
-public static void main(String args[]){
   // printing hello world
 public static void main(String args[]){
->>>>>>> 41906a76c3a351719f6834a75c00ad749e8c3599:hellow_world.java
 System.out.println("hello world");
 //   added one more commment
   int a = "abc";
